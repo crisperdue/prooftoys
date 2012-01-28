26eae5a642b78f1b8d47f1f3820ca965507785fe
Cris Perdue <cris@perdues.com>
2012-01-27 19:30:46 -0800

    Add Expr.freshen method to support sample proof step counts properly.

--- expr.js
+++ expr.js
@@ -280,6 +280,32 @@ Expr.prototype.justify = function(ruleName, ruleArgs, ruleDeps) {
 };
 
 /**
+ * If this is a step, return a copy with only a new ordinal.  The key
+ * point is that the result gets a fresh ordinal from the stepCounter.
+ * Intended for use in proof samples, to keep the ordinals of sample
+ * args near all the rest.
+ *
+ * TODO: Consider measuring numbers of steps differently, e.g. by
+ * capturing stepCounter before and after rule execution.
+ */
+Expr.prototype.freshen = function() {
+  if (this.isStep()) {
+    var expr = this.dup();
+    // Mark the step as having hypotheses if it is suitable.
+    // Some inference steps below the level of "replace" can bring
+    // back the LHS to being a set of hypotheses.
+    expr.hasHyps = this.hasHyps;
+    expr.ruleName = this.ruleName;
+    expr.ruleArgs = this.ruleArgs;
+    expr.ruleDeps = this.ruleDeps;
+    expr.ordinal = stepCounter++;
+    return expr;
+  } else {
+    return this;
+  }
+};
+
+/**
  * Match this expr against a schema, which may be given as a string.
  * See function matchAsSchema.
  */
@@ -2484,6 +2510,8 @@ var utils = {
   },
 
   // This calls a function with any number of arguments.
+  // TODO: Eliminate in favor of infixCall.  This will be problematic
+  // for some infix operators.
   call: function(fn, arg) {
     if ((typeof fn) == 'string') {
       fn = new Y.Var(fn);
