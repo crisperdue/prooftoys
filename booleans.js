99c7df3697e0e5e78d5323896ac36038f15f5d19
Cris Perdue <cris@perdues.com>
2012-06-30 13:22:36 -0700

    Connect truthtable highlighting to the pictures near the tables.

--- booleans.js
+++ booleans.js
@@ -19,22 +19,42 @@ function setClass(j, className, value) {
 }
 
 /**
- * Given a TABLE element of a truth table and boolean values for one
- * or two inputs, highlights the matching truth table cell plus its
- * row and column header cells by setting up cellHighlight and
- * thHighlight classes on the TD elements.
+ * Install event handlers to highlight cells in the jquery table
+ * element based on mouse position in the canvas relative to the
+ * circles.
+ */
+function installCircleHighlighting(table, canvas, circle1, circle2) {
+  var cxt = canvas.getContext('2d');
+  canvas.onmousemove = function(event) {
+    var p = isInside(cxt, eventPoint(event.target, event), circle1);
+    var q = (circle2
+             ? isInside(cxt, eventPoint(event.target, event), circle2)
+             : false);
+    highlightCurrentCell(table, p, q);
+  };
+  canvas.onmouseout = function(event) {
+    table.find('td, th')
+      .removeClass('cellHighlight thHighlight');
+  };
+}
+
+/**
+ * Given a jquery table element for a truth table and boolean values
+ * for one or two inputs, highlights the matching truth table cell
+ * plus its row and column header cells by setting up cellHighlight
+ * and thHighlight classes on the TD elements.
  */
 function highlightCurrentCell(table, xCircle, yCircle) {
   table.find('td').each(function() {
       var yCell = this.getAttribute('y');
       // Y value is OK if it matches or there is none.
       var yMatch = yCell == null || yCell == '' + yCircle;
-      setClass($(this),
+      setClass(jQuery(this),
                'cellHighlight',
                yMatch && this.getAttribute('x') == '' + xCircle);
     });
   table.find('th').each(function() {
-      setClass($(this), 'thHighlight',
+      setClass(jQuery(this), 'thHighlight',
                (this.hasAttribute('x')
                 ? this.getAttribute('x') == '' + xCircle
                 : this.getAttribute('y') == '' + yCircle));
@@ -111,53 +131,49 @@ function drawBooleans() {
       render(cxtNotA, merge(greenCircle, {fillStyle: '#eee'}));
       render(cxtNotA, merge(greenCircle, outsideOrange));
     });
+  var table = $('#negationTable');
+  installCircleHighlighting(table, cxtNotA.canvas, greenCircle);
 
   var cxtNotB = initCxt('canvasNotB');
   withinCircle(cxtNotB, function() {
       render(cxtNotB, merge(blueCircle, {fillStyle: '#eee'}));
       render(cxtNotB, merge(blueCircle, outsideOrange));
     });
+  installCircleHighlighting(table, cxtNotB.canvas, blueCircle);
 
   var cxtNotC = initCxt('canvasNotC');
   withinCircle(cxtNotC, function() {
       render(cxtNotC, merge(yellowCircle, {fillStyle: '#eee'}));
       render(cxtNotC, merge(yellowCircle, outsideOrange));
     });
+  installCircleHighlighting(table, cxtNotC.canvas, yellowCircle);
 
-  var byId = function(id) { return document.getElementById(id); };
-  var dSpan = byId('inD');
-  var eSpan = byId('inE');
-  var spanOr = byId('inDOrE');
-  var spanAnd = byId('inDAndE');
   var cxtOrDE = initCxt('canvasOrDE');
-  cxtOrDE.canvas.onmousemove = function(event) {
-    var d = isInside(cxt, eventPoint(cxtOrDE.canvas, event), circleD);
-    var e = isInside(cxt, eventPoint(cxtOrDE.canvas, event), circleE);
-    var table = $('table.disjunction1');
-    highlightCurrentCell(table, d, e, d || e);
-  };
-  cxtOrDE.canvas.onmouseout = function(event) {
-    $('table.disjunction1 td, table.disjunction1 th')
-      .removeClass('cellHighlight thHighlight');
-  };
   withinCircle(cxtOrDE, function() {
       render(cxtOrDE, circleD);
       render(cxtOrDE, circleE);
     });
+  var table = $('#disjunctionTable');
+  installCircleHighlighting(table, cxtOrDE.canvas, circleD, circleE);
 
   var cxtOrDE2 = initCxt('canvasOrDE2');
   withinCircle(cxtOrDE2, function() {
       render(cxtOrDE2, merge(circleD, {fillStyle: {image: shadeBlue}}));
       render(cxtOrDE2, merge(circleE, {fillStyle: {image: shadeBlue}}));
     });
+  installCircleHighlighting(table, cxtOrDE2.canvas, circleD, circleE);
 
   var cxtAndDE = initCxt('canvasAndDE');
   drawAnd(cxtAndDE,
 	  merge(circleD, {fillStyle: {image: shadeBlue}}),
 	  merge(circleE, {fillStyle: {image: shadeBlue}}));
+  installCircleHighlighting($('#conjunctionTable'),
+                            cxtAndDE.canvas, circleD, circleE);
 
   var cxtAndDE2 = initCxt('canvasAndDE2');
   drawAnd(cxtAndDE2, circleD2, circleE2);
+  installCircleHighlighting($('#conjunctionTable'),
+                            cxtAndDE2.canvas, circleD2, circleE2);
 }
 
 var birds = {
@@ -253,9 +269,9 @@ function drawStatements() {
   // Venn diagram of a false implication, coloring in just the
   // area where the implication is false.
   c = initCxt('canvasNotImplies');
+  var birds2 = merge(birds, {x: 85, noFill: true});
   withinCircle(c, function() {
       render(c, merge(wings, noFillStyle, {labelX: 160}));
-      var birds2 = merge(birds, {x: 85, noFill: true});
       render(c, birds2);
       withClipping(c, {shape: canvas, fillStyle: 'orange'},
                    merge(wings, {outside: true}),
@@ -268,6 +284,8 @@ function drawStatements() {
       render(c, merge(birds, {outside: true}));
       render(c, wings);
     });
+  installCircleHighlighting($('#implicationTable'),
+                            c.canvas, birds, wings);
 
   // Picture where bird --> wings is not true everywhere, with normal shading.
   c = initCxt('birdWingsFalse');
@@ -275,5 +293,7 @@ function drawStatements() {
       render(c, merge(birds, {outside: true, x: 77, labelX: 83}));
       render(c, merge(wings, {labelX: 160}));
     });
+  installCircleHighlighting($('#implicationTable'),
+                            c.canvas, birds2, wings);
 }
 
