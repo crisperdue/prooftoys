5ca623ead77ddd9fd217c624db7c24af45b7b7aa
Cris Perdue <cris@perdues.com>
2013-01-03 15:33:02 -0800

    Fix the TOC code to be more robust to possible situations.

--- jquery.tableofcontents.js
+++ jquery.tableofcontents.js
@@ -8,34 +8,40 @@
     // Use this to assign a unique name to each anchor created.
     var anchor_counter = 1;
     // Level established by a seen header tag.
-    var current_level = null;
+    var current_level;
+    // Level of first seen header tag.
+    var base_level = null;
     // Will contain the HTML text for the TOC.
     var toc_text = "";
       
     // Get the desired header elements.
     $('h1, h2, h3, h4, h5, h6').not(settings.exclude).each (function () {
       header_level = Number(this.tagName.substr(1,1));
-
-      // TODO: Improve the following code to properly handle transitions
-      // from level N+2 back to level N, where level N _does_ have an
-      // entry in the TOC.  Use a stack of old levels.
-      if (!current_level) {
-        // Do nothing.
-      } else if (current_level < header_level) {
-        toc_text += "<ul>";
-      } else if (current_level > header_level) {
-        toc_text += "</ul></li>";
-      } else if (current_level == header_level) {
-        toc_text += "</li>";
+      // Establish the base level from the first seen heading.
+      if (!base_level) {
+        base_level = header_level;
+        current_level = base_level;
       }
-      // Remember the current level.
-      current_level = header_level;
+      // Adjust the level to be at least the base.
+      if (header_level < base_level) {
+        header_level = base_level;
+      }
+      // Start or end lists to reach the level of this heading.
+      while (current_level < header_level) {
+        toc_text += '<ul>';
+        current_level++;
+      }
+      while (current_level > header_level) {
+        toc_text += '</ul>';
+        current_level--;
+      }
 
+      // Get the plain text of the header element.
+      header_text = $(this).text();
       // Use the ID as the name if it has one, otherwise generate one.
       var toc_name = $(this).attr('id') || 'toc_' + anchor_counter;
-      // Plain text of the element.
-      header_text = $(this).text();
-      // Start a list item.
+      // Add a list item with that text and a link to the named anchor.
+      // TODO: properly escape the header text.
       toc_text += '<li><a href="#' + toc_name + '">' + header_text + '</a>';
 
       // Add an anchor before the header element.
@@ -43,6 +49,9 @@
       // Increment the counter in every case.
       anchor_counter++;
     });
+    while (current_level > base_level) {
+      toc_text += '</ul>';
+    }
     // Add the TOC.
     $(this).append('<ul>' + toc_text + '</ul>');
     return $(this);
