472c10953819f31fec58c8fb0c43349d0eb45dee
Cris Perdue <cris@perdues.com>
2013-01-05 08:47:14 -0800

    Use HTML rather than text contents of heading elements

--- jquery.tableofcontents.js
+++ jquery.tableofcontents.js
@@ -12,7 +12,7 @@
     // Level of first seen header tag.
     var base_level = null;
     // Will contain the HTML text for the TOC.
-    var toc_text = "";
+    var toc_html = "";
       
     // Get the desired header elements.
     $('h1, h2, h3, h4, h5, h6').not(settings.exclude).each (function () {
@@ -28,21 +28,21 @@
       }
       // Start or end lists to reach the level of this heading.
       while (current_level < header_level) {
-        toc_text += '<ul>';
+        toc_html += '<ul>';
         current_level++;
       }
       while (current_level > header_level) {
-        toc_text += '</ul>';
+        toc_html += '</ul>';
         current_level--;
       }
 
-      // Get the plain text of the header element.
-      header_text = $(this).text();
+      // Get the HTML contents of the heading.
+      header_html = $(this).html();
       // Use the ID as the name if it has one, otherwise generate one.
       var toc_name = $(this).attr('id') || 'toc_' + anchor_counter;
       // Add a list item with that text and a link to the named anchor.
       // TODO: properly escape the header text.
-      toc_text += '<li><a href="#' + toc_name + '">' + header_text + '</a>';
+      toc_html += '<li><a href="#' + toc_name + '">' + header_html + '</a>';
 
       // Add an anchor before the header element.
       $(this).before('<a name="' + toc_name + '"></a>');
@@ -50,10 +50,10 @@
       anchor_counter++;
     });
     while (current_level > base_level) {
-      toc_text += '</ul>';
+      toc_html += '</ul>';
     }
     // Add the TOC.
-    $(this).append('<ul>' + toc_text + '</ul>');
+    $(this).append('<ul>' + toc_html + '</ul>');
     return $(this);
   }
 }) (jQuery);
