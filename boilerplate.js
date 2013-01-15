6114bec54bc1b456007fbe2d559fc486e2773828
Cris Perdue <cris@perdues.com>
2013-01-15 09:55:39 -0800

    Support "!=" and "-" in Toy.mathMarkup.

--- boilerplate.js
+++ boilerplate.js
@@ -86,13 +86,15 @@ Toy.mathMarkup = function(text) {
   // Substitutions for "forall" and "exists" consume a trailing blank,
   // helping to push them up next to following text.
   var rex =
-    /==>|==|>=|<=|[*]|\bforall( |\b)|\bexists( |\b)|[_a-zA-Z][_a-zA-Z0-9]*/g;
+    /==>|==|!=|>=|<=|-|[*]|\bforall( |\b)|\bexists( |\b)|[_a-zA-Z][_a-zA-Z0-9]*/g;
   return text.replace(rex, function(s) {
     switch(s) {
     case '==>': return '&rArr;';
     case '==': return '&equiv;';
+    case '!=': return '&ne;';
     case '>=': return '&ge;';
     case '<=': return '&le;';
+    case '-': return '&minus;';
     case '*': return '&sdot;';
     case 'forall': return '&forall;';
     case 'forall ': return '&forall;';
