11e0c8ba7dc9f14426fc9b45ef16338bbae920f0
Cris Perdue <cris@perdues.com>
2013-05-13 14:56:04 -0700

    Shorten display of steps using rule rewriteWithFact; use rewrite-style hovers for them.

--- theorems.js
+++ theorems.js
@@ -2276,7 +2276,8 @@ var ruleInfo = {
     // TODO: Consider giving this a form.  Entry for facts would
     // probably need to allow omission of type info.
     inputs: {site: 1, term: 3},
-    description: 'rewrite {site};; {in step siteStep} using {shortFact}',
+    isRewriter: true,
+    description: '{in step siteStep} use;; {shortFact}'
   },
 
   // Variant of "rewrite" that simplifies numeric types after applying
