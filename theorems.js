17194a88d5215953c6e9d599a421ed5bb2b1de30
Cris Perdue <cris@perdues.com>
2013-09-25 14:05:48 -0700

    This time really make rules.display work as intended.

--- theorems.js
+++ theorems.js
@@ -206,7 +206,9 @@ var ruleInfo = {
    */
   display: {
     action: function(step) {
-      return step.justify('display', arguments, [step]);
+      // Call copyStep to persuade Expr.justify not to entirely
+      // ignore this step.
+      return step.copyStep().justify('display', arguments, [step]);
     },
     inputs: {step: 1},
     form: ('Does nothing, but result will display in full. '
