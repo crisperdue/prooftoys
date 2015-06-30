48423b26265c985cd878f2bab58afb66b9edc309
Cris Perdue <cris@perdues.com>
2015-06-29 21:36:42 -0700

    Remove old throwaway scripts.

--- ors.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Search for occurrences of single "|" characters.
-grep -P "(?<![|])[|](?![|])" $(ls *.js *.html | grep -v jquery-)
--- replace-ops.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Replace occurrences of || followed by an odd number of single quotes
-# Multiple occurrences can be replaced per line.
-perl -i -ple "s{\|\|(?=[^']*'([^']*'[^']*')*[^']*\$)}{|}g" *.js tests.html
-# Similarly for &&
-perl -i -ple "s{&&(?=[^']*'([^']*'[^']*')*[^']*\$)}{&}g" *.js tests.html
