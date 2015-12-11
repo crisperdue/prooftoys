64a47adbeb2c00395cdddca689058638da7d344d
Cris Perdue <cris@perdues.com>
2015-12-10 18:03:58 -0800

    Comments.

--- js/engine.js
+++ js/engine.js
@@ -1,5 +1,9 @@
 // Copyright 2011, 2012, 2013, 2014, 2015 Crispin Perdue.
 // All rights reserved.
+//
+// This file implements the logic, its axioms, basic theorems,
+// inference rules and other machinery that is relatively
+// independent of the problem domain.
 
 // Set all of this up immediately upon load, but avoiding changes
 // to the global environment (except through the "Toy" namespace).
