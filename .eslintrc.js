80d6ca7e0350f270c473d742a79e6cefe4ac5cf8
Cris Perdue <cris@perdues.com>
2017-01-10 18:06:48 -0800

    Add .eslintrc.js.

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Toy": true
+    },
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
