b36d304249c19159684ca9da0ef1a9e83b2817f8
Cris Perdue <cris@perdues.com>
2018-05-25 12:07:46 -0700

    Add Toy.copyToClipboard.

--- js/util.js
+++ js/util.js
@@ -1812,6 +1812,44 @@ FakeRpcWorker.prototype.postMessage = function(wrapper) {
 }
 
 
+/**
+ * Copies a string to the clipboard. Must be called from within an 
+ * event handler such as click. May return false if it failed, but
+ * this is not always possible. Browser support for Chrome 43+, 
+ * Firefox 42+, Safari 10+, Edge and IE 10+.
+ * IE: The clipboard feature may be disabled by an administrator. By
+ * default a prompt is shown the first time the clipboard is 
+ * used (per session).
+ *
+ * From: https://stackoverflow.com/questions/400212/
+ *   how-do-i-copy-to-the-clipboard-in-javascript
+ */
+function copyToClipboard(text) {
+  if (window.clipboardData && window.clipboardData.setData) {
+    // IE specific code path to prevent textarea being shown while
+    // dialog is visible.
+    return clipboardData.setData("Text", text); 
+  } else if (document.queryCommandSupported &&
+             document.queryCommandSupported("copy")) {
+    var textarea = document.createElement("textarea");
+    textarea.textContent = text;
+    // Prevent scrolling to bottom of page in MS Edge.
+    textarea.style.position = "fixed";
+    document.body.appendChild(textarea);
+    textarea.select();
+    try {
+      // Security exception may be thrown by some browsers.
+      return document.execCommand("copy");
+    } catch (ex) {
+      console.warn("Copy to clipboard failed.", ex);
+      return false;
+    } finally {
+      document.body.removeChild(textarea);
+    }
+  }
+}
+
+
 //// Export public names.
 
 Toy.hasOwn = hasOwn;
@@ -1901,6 +1939,8 @@ Toy.logRpc = false;
 Toy.MessageQueue = MessageQueue;
 Toy.FakeRpcWorker = FakeRpcWorker;
 Toy.makeRpcPromise = makeRpcPromise;
+
+Toy.copyToClipboard = copyToClipboard;
 
 // for interactive testing
 Toy._testTriangle = _testTriangle;
