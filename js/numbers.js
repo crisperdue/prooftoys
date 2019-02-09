45dff1d9475df1082d394efeae589d0f5bc1783d
Cris Perdue <cris@perdues.com>
2019-02-09 11:05:24 -0800

    Prove minv 0 = none.

--- js/numbers.js
+++ js/numbers.js
@@ -270,6 +270,27 @@ const inverses =
      '(7 display (s 6))',
      '(8 removeLet (s 7) (path "/left/left/right"))'
      ]
+   },
+
+   {statement: 'minv 0 = none',
+    proof:
+    [
+     '(1 definition "mulInverses" "")',
+     '(2 applyBoth (s 1) (t 0))',
+     '(3 apply (s 2) (path "/main/right"))',
+     '(4 fact "0 != 1")',
+     '(5 fact "0=0*x")',
+     '(6 replace (s 4) (path "/main/left") (s 5))',
+     '(7 rewrite (s 6) (path "/right") (t ((a != b) == (not (a = b)))))',
+     '(8 rewrite (s 7) (path "") (t ((a => (not b)) == ((a & b) == F))))',
+     '(9 rewrite (s 3) (path "/main/right/body") (t (((a & b) & c) == (a & (b & c)))))',
+     '(10 rewriteFrom (s 9) (path "/main/right/body/right") (s 8))',
+     '(11 simplifySite (s 10) (path "/main/right/body"))',
+     '(12 rewrite (s 11) (path "/main/right") (t ({x. F} = empty)))',
+     '(13 applyToBoth (t the1) (s 12))',
+     '(14 rewrite (s 13) (path "/main/left") (t ((the1 (mulInverses x)) = (minv x))))',
+     '(15 rewrite (s 14) (path "/main/right") (t ((the1 empty) = none)))'
+     ]
    }
    ];
 addRules(inverses);
