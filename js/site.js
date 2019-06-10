67cfb80947bf73d246f12f267d134ef8b57f86ad
Cris Perdue <cris@perdues.com>
2019-06-10 16:30:26 -0700

    Remove Google Analytics support from site.js. It is now handled in a page layout file.

--- js/site.js
+++ js/site.js
@@ -46,19 +46,6 @@ Toy.insertSlogans = function() {
   }
 };
 
-// Google analytics support.
-if (!location.pathname.match(/\/tests.html$/)) {
-  var id = (location.hostname == 'prooftoys.org'
-            ? 'UA-28801147-2' : 'UA-28801147-3');
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', id, 'auto');
-  ga('send', 'pageview');
-}
-
 //
 // Generating math symbols and italicizing alphabetic characters for math.
 //
