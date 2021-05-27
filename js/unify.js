b88834b9d15bd31a9cb841225a2e325ab755a826
Cris Perdue <cris@perdues.com>
2021-05-27 09:38:25 -0700

    Update unification and tests.

--- js/unify.js
+++ js/unify.js
@@ -1,4 +1,6 @@
-// Copyright Crispin Perdue.  All rights reserved.
+//// Copyright Crispin Perdue.  All rights reserved.
+
+//// Unification in the style of John Harrison's "Practical Logic".
 
 'use strict';
 
@@ -11,14 +13,15 @@ const Bindings = Toy.Bindings;
 const findBinding = Toy.findBinding;
 
 // Given bindings of names to terms, a variable name, and a term,
-// returns true iff the variable matches the term, after applying (one
-// of) the bindings if necessary.  This result indicates that the
-// current bindings are sufficient to match the variable with the
-// term.  Otherwise false.
+// returns true iff the variable matches the term after applying the
+// bindings zero or more times.  This indicates that the current
+// bindings are sufficient to match the variable with the term.
+// Otherwise the result is false.
 //
-// Also detects if the new match would introduce circularity, and in
-// this case throws an Error.
-function isTriv(bind, name, term) {
+// If the term is composite, this applies the bindings to its parts
+// recursively, and if it ever reaches the same variable, it fails by
+// invoking the exit with the false value.
+function isTriv(bind, name, term, exit) {
   if (term instanceof TypeVar) {
     const n2 = term.name;
     if (name === n2) {
@@ -28,58 +31,105 @@ function isTriv(bind, name, term) {
       return !!b && isTriv(bind, name, b.to);
     }
   } else if (term instanceof FuncType) {
-    return (isTriv(bind, name, term.types[0]) ||
-            isTriv(bind, name, term.types[1])
-            ? Toy.abort('cyclic')
+    return (isTriv(bind, name, term.types[0], exit) ||
+            isTriv(bind, name, term.types[1], exit)
+            ? exit(false)
             : false);
   } else {
     return false;
   }
 }
 
-function unify(bind, pairs) {
-  if (pairs == null) {
-    return bind;
-  } else {
-    const unifVar = (v, term) => {
-      const name = v.name;
-      const found = Toy.findBinding(name, bind);
-      if (found) {
-        return unify(bind, new Bindings(found.to, term, pairs.more));
+// Returns bindings if the pairs unify (possibly null if
+// no substitution is needed); or false if they do not unify.
+function coreUnify(bind_arg, pairs_arg) {
+  return Toy.withExit(exit => {
+    function unite(bind, pairs) {
+      if (pairs == null) {
+        return bind;
       } else {
-        return unify(isTriv(bind, name, term)
-                     ? bind
-                     : new Bindings(name, term, bind),
-                     pairs.more);
+        const unifVar = (v, term) => {
+          const name = v.name;
+          const found = Toy.findBinding(name, bind);
+          if (found) {
+            return unite(bind, new Bindings(found.to, term, pairs.more));
+          } else {
+            return unite(isTriv(bind, name, term, exit)
+                          ? bind
+                          : new Bindings(name, term, bind),
+                          pairs.more);
+          }
+        };
+        const x = pairs.from;
+        const y = pairs.to;
+        const xt = x.constructor;
+        const yt = y.constructor;
+        if (xt === TypeConst && yt === TypeConst && x === y) {
+          return unite(bind, pairs.more);
+        } else if (xt === TypeVar) {
+          return unifVar(x, y);
+        } else if (yt === TypeVar) {
+          return unifVar(y, x);
+        } else if (xt === FuncType && yt === FuncType) {
+          const newPairs =
+                new Bindings(x.types[1], y.types[1],
+                             new Bindings(x.types[0], y.types[0], pairs.more));
+          return unite(bind, newPairs);
+        } else {
+          return false;
+        }
       }
     }
-    const x = pairs.from;
-    const y = pairs.to;
-    const xt = x.constructor;
-    const yt = y.constructor;
-    if (xt === TypeConst && yt === TypeConst && x === y) {
-      return unify(bind, pairs.more);
-    } else if (xt === TypeVar) {
-      return unifVar(x, y);
-    } else if (yt === TypeVar) {
-      return unifVar(y, x);
-    } else if (xt === FuncType && yt === FuncType) {
-      const newPairs =
-            new Bindings(x.types[0], y.types[0],
-                         new Bindings(x.types[1], y.types[1], pairs.more));
-      return unify(bind, newPairs);
-    } else {
-      Toy.abort('No unifier');
-    }
-  }
+    return unite(bind_arg, pairs_arg);
+  });
 }
 
-function solve(bind) {
-  return;
+// This is substitution into a type term.
+function tsubst(bind, tm) {
+  const type = tm.constructor;
+  if (type === TypeVar) {
+    const found = Toy.findBinding(tm.name, bind);
+    if (found) {
+      return found.to;
+    }
+  } else if (type === FuncType) {
+    const t1 = tm.types[1];
+    const t0 = tm.types[0];
+    const newT1 = tsubst(bind, tm.types[1]);
+    const newT0 = tsubst(bind, tm.types[0]);
+    if (newT1 !== t1 || newT0 !== t0) {
+      return new FuncType(newT1, newT0);
+    }
+  }
+  return tm;
+}
+
+// Converts the result of "coreUnify" into a single substitution that does
+// the entire work.
+function resolve(bind) {
+  if (bind) {
+    let changed;
+    // Continue doing rounds of substitution until no term changes.
+    do {
+      changed = false;
+      let bind2 = null;
+      for (const [nm, term] of bind) {
+        const term2 = tsubst(bind, term);
+        // This test presumes that a no-op substitution
+        // returns its input, not a copy.
+        if (term2 !== term) {
+          changed = true;
+        }
+        bind2 = new Bindings(nm, term2, bind2);
+      }
+      bind = bind2;
+    } while (changed);
+  }
+  return bind;
 }
 
 function fullUnify(term1, term2) {
-
+  return resolve(unif2(term1, term2));
 }
 
 // For debugging: parses a string representing
@@ -90,15 +140,20 @@ function pt(tterm) {
 
 // For debugging and perhaps other purposes; returns a Map with the
 // bindings from unifying just term1 and term2.
-function unif1(term1, term2) {
-  return Toy.unify(null, new Toy.Bindings(term1, term2, null));
+function unif2(term1, term2) {
+  return coreUnify(null, new Toy.Bindings(term1, term2, null));
 }
 
+// TODO: Define unif2Map, that takes string inputs and returns a Map
+//   with the bindings in order of their creation; then interactively
+//   test it;
+
+Toy.tsubst = tsubst;
 Toy.isTriv = isTriv;
-Toy.unify = unify;
-Toy.solve = solve;
+Toy.coreUnify = coreUnify;
+Toy.resolve = resolve;
 Toy.fullUnify = fullUnify;
 Toy.pt = pt;
-Toy.unif1 = unif1;
+Toy.unif2 = unif2;
 
 })();
--- tests/unify-tests.js
+++ tests/unify-tests.js
@@ -7,21 +7,52 @@
   const Bindings = Toy.Bindings;
 
   const pt = Toy.parseType;
-  const unif1 = Toy.unif1;
+  const unif2 = Toy.unif2;
   const isTriv = Toy.isTriv;
-  const unify = Toy.unify;
-  const solve = Toy.solve;
+  const coreUnify = Toy.coreUnify;
+  const resolve = Toy.resolve;
   const fullUnify = Toy.fullUnify;
+  
+  // This converts a Bindings result of unification into an array of
+  // strings; if null, i.e. no bindings, an empty array.  If false,
+  // then false.
+  function asArray(bb) {
+    if (bb === false) {
+      return false;
+    }
+    const result = [];
+    if (bb) {
+      for (const [k, v] of bb) {
+        // The first binding encountered was added last, so put it at
+        // the end of the array.
+        result.unshift([k, v && v.toString()]);
+      }
+    }
+    return result;
+  }
+
+  // Builds and returns bindings from the given array of from, to
+  // pairs, with both "from" and "to" given as strings.
+  function newB(pairs) {
+    let result = null;
+    for (const [from, to] of pairs) {
+      result = new Bindings(from, pt(to), result);
+    }
+    return result;
+  }
+
+  function unif2Array(a, b) {
+    const bind = unif2(pt(a), pt(b));
+    // console.log('' + bind);
+    return asArray(bind);
+  }
 
   const bind1 = new Bindings('t1', pt('i'));
   const bind2 = new Bindings('t2', pt('o i'));
 
-  window.bind1 = bind1;
-  window.bind2 = bind2;
+  const tests = [
 
-  const tests = {
-
-    tokTypes: a => {
+    function testTokenizing(a) {
       // This is an array of the names of all of the tokens.
       const tt = str => Toy.tokenizeType(str).map(x => x.name);
       const equal = a.deepEqual.bind(a);
@@ -32,11 +63,29 @@
       equal(tt(' asdf10'), ['asdf10']);
       equal(tt('o i'), ['o', 'i']);
       equal(tt('(o i)'), ['(', 'o', 'i', ')']);
-      equal(tt('(o i)(o i)'), ['(', 'o', 'i', ')', '(', 'o', 'i', ')']);
-      equal(tt('  o    i )(   ('), ['o', 'i', ')', '(', '(']);
+      equal(tt('(o i)(o i)'),
+            ['(', 'o', 'i', ')', '(', 'o', 'i', ')']);
+      // Tokenize an ill-formed string.
+      equal(tt('  o    i )(   ('),
+            ['o', 'i', ')', '(', '(']);
     },
 
-    testIsTriv: a => {
+    function testTsubst(a) {
+      const ts = (bind, termStr) => 'foo';
+      a.ok(ts(1, 2));
+    },
+
+    function testNewB(a) {
+      const a1 = [['t1', 'i'], ['t2', 't3']];
+      const b1 = newB(a1);
+      a.deepEqual(asArray(b1), a1);
+      const a2 = [];
+      const b2 = newB(a2);
+      a.equal(b2, null);
+      a.deepEqual(asArray(b2), a2);
+    },
+
+    function testIsTriv(a) {
       a.ok(isTriv(null, 't', pt('t')));
       a.notOk(isTriv(null, 't', pt('o')));
       a.notOk(isTriv(null, 't', pt('o i')));
@@ -45,46 +94,74 @@
       a.notOk(isTriv(null, 't', pt('o t2')));
     },
 
-    testUnif1: a => {
+    function testUnif2a(a) {
       const equal = a.deepEqual.bind(a);
-      // This converts a Bindings into an array of strings; if null,
-      // i.e. no bindings, an empty array.
-      const asArray = bb => {
-        const result = [];
-        if (bb) {
-          for (const [k, v] of bb) {
-            result.push(k, v && v.toString());
-          }
-        }
-        return result;
-      };
-      const array1 = (a, b) => {
-        const bind = unif1(pt(a), pt(b));
-        console.log('' + bind);
-        return asArray(bind);
-      };
-      equal(array1('i', 'i'), []);
-      equal(array1('t', 't'), []);
-      equal(array1('t', 'i'), ['t', 'i']);
-      equal(array1('o', 't1'), ['t1', 'o']);
-      equal(array1('t', 'o i'), ['t', '(o i)']);
+      equal(unif2Array('i', 'i'), []);
+      equal(unif2Array('t', 't'), []);
+      equal(unif2Array('t', 'i'), [['t', 'i']]);
+      equal(unif2Array('o', 't1'), [['t1', 'o']]);
+      equal(unif2Array('t', 'o i'), [['t', '(o i)']]);
+
       // Ordering of the result pair can be treated as arbitrary,
       // but it may be useful to predict it.
-      equal(array1('t1', 't2'), ['t1', 't2']);
-      equal(array1('t2', 't1'), ['t2', 't1']);
-    }
+      equal(unif2Array('t1', 't2'), [['t1', 't2']]);
+      equal(unif2Array('t2', 't1'), [['t2', 't1']]);
+    },
 
-  };
+    function testUnif2b(a) {
+      const equal = a.deepEqual.bind(a);
+      // Constants mismatch
+      equal(unif2Array('((o i) t1 t2)', '(i t2 t1)'), false);
+      // Cyclic
+      equal(unif2Array('t', 'o t'), false);
+      equal(unif2Array('t t', 't (o t)'), false);
+      equal(unif2Array('t t', 't (t o)'), false);
+
+
+      equal(unif2Array('(o i) t1 t2', '(o i) t2 t1'), [['t1', 't2']]);
+      equal(unif2Array('i t1 (i t2)', 'i (i t2) t1'),
+            [['t1', '(i t2)']]);
+      equal(unif2Array('i t1 (i t2)', 'i (i t3) t4'),
+            [['t1', '(i t3)'], ['t4', '(i t2)']]);
+    },
+
+    // These give results that are not resolved.
+    function testUnif2c(a) {
+      const check = (t1, t2, expected) =>
+            a.deepEqual(unif2Array(t1, t2), expected);
+
+      check('t1 t1', 't2 i', [['t1', 't2'], ['t2', 'i']]);
+      check('t2 i', 't1 t1', [['t2', 't1'], ['t1', 'i']]);
+      check('t t t', 't1 (t2 t2) t1',
+            [['t', 't1'], ['t1', '(t2 t2)']]);
+      check('(t1 t2) (t2 t1)', 't3 t3',
+            [['t3', '(t1 t2)'], ['t1', 't2']]);
+    },
+
+    // Like testUnif2c, but with results resolved.
+    function testUnif2d(a) {
+      const check = (t1, t2, expected) =>
+            a.deepEqual(asArray(resolve (unif2(pt(t1), pt(t2)))),
+                        expected);
+
+      check('t1 t1', 't2 i', [['t1', 'i'], ['t2', 'i']]);
+      check('t2 i', 't1 t1', [['t2', 'i'], ['t1', 'i']]);
+      check('t t t', 't1 (t2 t2) t1',
+            [['t', '(t2 t2)'], ['t1', '(t2 t2)']]);
+      check('(t1 t2) (t2 t1)', 't3 t3',
+            [['t3', '(t2 t2)'], ['t1', 't2']]);
+    },
+
+  ];
 
   // Run them:
 
   QUnit.module('Unif');
 
-  const todos = new Set();
-  for (var name in tests) {
-    const fn = tests[name];
+  for (var fn of tests) {
+    const name = fn.name;
     if (typeof fn === 'function') {
-      if (todos.has(name)) {
+      if (name.startsWith('TODO')) {
         QUnit.todo(name, fn);
       } else {
         QUnit.test(name, fn);
@@ -93,5 +170,13 @@
       console.log(name + ': not a function');
     }
   }
+
+  // Support interactive testing (temporarily):
+
+  window.aa = asArray;
+  window.newB = newB;
+
+  window.bind1 = bind1;
+  window.bind2 = bind2;
 
 }();
