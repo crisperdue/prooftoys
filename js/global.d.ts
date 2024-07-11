d38b2b9f7eeeeceffe046da71829cf420bed745f
Cris Perdue <cris@perdues.com>
2024-07-11 13:35:50 -0700

    Update global info.

--- js/global.d.ts
+++ js/global.d.ts
@@ -3,11 +3,19 @@
 interface Window {
   _paq: Array;
   $: function;
+  proofDisplay: any;
   proofEditor: any;
   rCounter: number;
   dCounter: number;
+  Numtest: function;
+  Tval: any;
 }
 
 interface Element {
+  onclick: function;
   handleResize: function;
+}
+
+interface Object {
+  $$: string;
 }
